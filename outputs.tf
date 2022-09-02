f0643781e12cc4706728fb60e1b4d1e1f1b56a13
Antony Bailey <antony_bailey@waters.com>
2022-09-02 09:46:33 +0100

    Add descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "cluster_name" {
   value = data.aws_eks_cluster.cluster.name
+  description = "The EKS Cluster we deployed Linkerd to."
 }
 
 output "cert_expiry" {
   value = tls_locally_signed_cert.cert.validity_end_time
+  description = "The time that the certificate we generated expires."
 }
